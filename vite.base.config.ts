86c6d9f2b084ac0a78cdaf8df3fd7106d8d80ec0
jerryzhang <jerryzhang@muchenglin.com>
2023-01-20 12:39:34 +0800

    fix

--- vite.base.config.ts
+++ vite.base.config.ts
@@ -1,33 +1,38 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import * as path from 'path'
-import { isVue2 } from 'vue-demi'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 const outputName = 'index'
-export const defaultPlugins = []
 
 // https://vitejs.dev/config/
 export const baseBuildConfig = defineConfig({
   build: {
-    outDir: path.resolve(__dirname, `./dist/${isVue2 ? 'v2' : 'v3'}`),
-    emptyOutDir: false,
+    outDir: 'lib',
+    emptyOutDir: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs', 'umd'],
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
       name: 'index',
       fileName: (format) => `${outputName}.${format}.js`
     },
     rollupOptions: {
-      external: ['vue', '@vue/composition-api/dist/vue-composition-api.mjs'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-          '@vue/composition-api/dist/vue-composition-api.mjs': 'VueCompositionAPI'
-        }
-      }
+      external: [
+        'vue',
+        'vue-demi',
+        '@auto-route/core',
+        '@auto-route/webpack-plugin'
+      ]
     }
   },
+  plugins: [
+    dts({
+      tsConfigFilePath: resolve(__dirname, './tsconfig.json'),
+      skipDiagnostics: false,
+      logDiagnostics: true,
+      insertTypesEntry: true
+    })
+  ],
   optimizeDeps: {
     exclude: ['vue-demi', 'vue', 'vue2']
   },
